dbc2be4819a711253f8a3293780e72883a2ec34c
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-03-16 18:19:10 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+
+function writeCards(name, eventName) {
+    let cards = []
+    for (let i = 0; i < name.length; i++) {
+        cards.push (`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`)
+    
+    
+    }
+
+
+return cards;
+}
+
+
+
+function countDown(number) {
+
+    while (number >= 0) {
+         
+        console.log(number);
+        number--;
+
+    }
+
+    return number;
+
+
+}
